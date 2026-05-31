16cc5469819498f0bd6ffe5aa08e1dbd1516b8e9
BotaPetre <@botapetre1@gmail.com>
2026-05-31 12:29:53 +0300

    add routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./core/auth/login-page/login-page').then(p => p.LoginPage)
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./core/auth/register-page/register-page').then(p => p.RegisterPage)
+    },
+    {
+        path: 'transactions',
+        loadChildren: () => import('./features/transaction/transactions.routes').then(r => r.transactionRoutes)
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./shared/components/not-found-page/not-found-page').then(p => p.NotFoundPage)
+    }, // Place last, otherwise overwrites all paths
+];
