a003bead667a8f26d835bbe532ba6c25efe7f56c
BotaPetre <@botapetre1@gmail.com>
2026-08-20 21:09:47 +0300

    alva version signals form

--- client/src/app/features/transaction/add-transaction/add-transaction.ts
+++ client/src/app/features/transaction/add-transaction/add-transaction.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { form, FormField, required, minLength, maxLength} from '@angular/forms/signals';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormItemComponent, NzFormLabelComponent, NzFormModule } from 'ng-zorro-antd/form';
+
+interface IAddTransaction {
+  createDate: string;
+  amount: number;
+  currency: string;
+  category: string;
+  note: string;
+}
 
 @Component({
   selector: 'add-transaction',
-  imports: [],
+  imports: [FormField, NzButtonModule, NzFormModule],
   templateUrl: './add-transaction.html',
   styleUrl: './add-transaction.css',
 })
 export class AddTransaction {
+
+  private addTransactionModel = signal<IAddTransaction>({
+    createDate: '',
+    amount: 0,
+    currency: 'RON',
+    category: '',
+    note: '',
+  });
+
+  trForm = form(this.addTransactionModel, (schemaPath) => {
+    required(schemaPath.createDate, {message: 'Date is required'});
+    required(schemaPath.amount, {message: 'Amount is required'});
+    required(schemaPath.currency, {message: 'Currency is required'});
+    required(schemaPath.category, {message: 'Category is required'});
+    minLength(schemaPath.category, 3, { message: 'Category must be at least 3 characters' });
+    maxLength(schemaPath.category, 256, { message: 'Category can have maximum 256 characters' });
+  });
+
+  submitNewTransaction(event: Event) {
+    event.preventDefault();
+     
+    console.log(this.trForm().value())
+  }
 
 }
--- client/src/app/features/transaction/transactions-page/transactions-page.ts
+++ client/src/app/features/transaction/transactions-page/transactions-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { AddTransaction } from "../add-transaction/add-transaction";
 
 @Component({
   selector: 'transactions-page',
-  imports: [NzButtonModule],
+  imports: [NzButtonModule, AddTransaction],
   templateUrl: './transactions-page.html',
   styleUrl: './transactions-page.css',
 })
