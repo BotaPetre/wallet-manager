5f89008f384b91717488ff54867d516139b70aa8
BotaPetre <@botapetre1@gmail.com>
2026-08-20 18:53:25 +0300

    add user schema and drizzle

---
+++ client/src/app/features/transaction/add-transaction/add-transaction.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'add-transaction',
+  imports: [],
+  templateUrl: './add-transaction.html',
+  styleUrl: './add-transaction.css',
+})
+export class AddTransaction {
+
+}
--- client/src/app/features/transaction/transactions-page/transactions-page.ts
+++ client/src/app/features/transaction/transactions-page/transactions-page.ts
@@ -2,16 +2,11 @@ import { Component } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
-  selector: 'app-transactions-page',
+  selector: 'transactions-page',
   imports: [NzButtonModule],
   templateUrl: './transactions-page.html',
   styleUrl: './transactions-page.css',
 })
 export class TransactionsPage {
-
-
-  test() {
-    alert("test");
-  }
 
 }
