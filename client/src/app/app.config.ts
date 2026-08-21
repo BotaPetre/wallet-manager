dc4d1f4154658b09df5cf55098b932cac7c64efc
BotaPetre <@botapetre1@gmail.com>
2026-08-21 05:10:50 +0300

    upgrade ng zoroo

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideNzDateFnsAdapter } from 'ng-zorro-antd/core/time';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes), provideNzDateFnsAdapter(),
   ]
 };
